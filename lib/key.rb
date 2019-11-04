f983320b2f4ff27a78db8fa6c40892f685c43474
Joshua O'bannon <jtobannon@gmail.com>
2019-11-03 17:48:56 -0700

    Expose key shift values in key class and tests/ moved logic from Engima

--- lib/key.rb
+++ lib/key.rb
@@ -1,14 +1,28 @@
 class Key
-  attr_reader :rand_num_string
+  attr_reader :rand_num_string, :a_key, :b_key, :c_key, :d_key
   def initialize
-      5.times do
-        rand_num = rand(5).to_s
-        rand_num_string = rand_num
-      end
-      rand_num_string
+    @a_key = 0
+    @b_key = 0
+    @c_key = 0
+    @d_key = 0
+
+    create_num_string
+    create_key
   end
 
+  def create_num_string
+    5.times do
+      rand_num = rand(5).to_s
+      @rand_num_string = rand_num
+    end
+    rand_num_string
+  end
 
-
+  def create_key
+    @a_key = @rand_num_string[0].to_i
+    @b_key = @rand_num_string[1].to_i
+    @c_key = @rand_num_string[2].to_i
+    @d_key = @rand_num_string[3].to_i
+  end
 
 end
--- test/key_test.rb
+++ test/key_test.rb
@@ -8,6 +8,12 @@ class KeyTest < Minitest::Test
   end
 
   def test_it_has_attributes
+    expected = 1 #not sure why cant do integer.new
+    assert_equal expected.class, @key.a_key.class
+    assert_equal expected.class, @key.b_key.class
+    assert_equal expected.class, @key.c_key.class
+    assert_equal expected.class, @key.d_key.class
+
     @key.expects(:rand_num_string).returns('12345')
     assert_equal '12345', @key.rand_num_string
   end
