5ed89cfe037f5da275ebee331aedc2039777e4aa
Joshua O'bannon <jtobannon@gmail.com>
2019-11-05 19:24:40 -0700

    work on encrypt and decrypt

--- lib/decrypt.rb
+++ lib/decrypt.rb
@@ -2,18 +2,26 @@
 require 'pry'
 require_relative './enigma'
 
-
+message_array = []
 file = File.open(ARGV[0], "r")
+file.readlines.each do |line|
+    message_array << line.chomp
+    binding.pry
+end
+file.close
 
 enigma = Enigma.new
-message = file.readlines.first.strip
-file.close
 
 filewriter = File.open(ARGV[1], 'w')
-decrypted_hash = enigma.decrypt(message, ARGV[2], ARGV[3])
-decrypted_hash[:encryption]
-filewriter.write(decrypted_hash[:decryption])
-puts "Created #{ARGV[1]} " +
-     "with the key #{decrypted_hash[:key]} " +
-     "and date #{decrypted_hash[:date]}"
-file.close
+message_array.each_with_index do |message, i|
+  decrypted_hash = enigma.decrypt(message, ARGV[2], ARGV[3])
+  if i == (message_array.size - 1)
+    binding.pry
+  end
+  filewriter.write(decrypted_hash[:decryption])
+  if i == (message_array.size - 1)
+    puts "Created #{ARGV[1]} " +
+         "with the key #{decrypted_hash[:key]} " +
+         "and date #{decrypted_hash[:date]}"
+  end
+end
