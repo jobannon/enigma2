c21f12f1829f51ed335bdaa7740a921e4d70dd52
Joshua O'bannon <jtobannon@gmail.com>
2019-11-05 21:55:12 -0700

    Refactor decrypted to spec/interaction pattern

--- lib/decrypt.rb
+++ lib/decrypt.rb
@@ -2,26 +2,15 @@
 require 'pry'
 require_relative './enigma'
 
-message_array = []
+enigma = Enigma.new
 file = File.open(ARGV[0], "r")
-file.readlines.each do |line|
-    message_array << line.chomp
-    binding.pry
-end
+message = file.read
 file.close
 
-enigma = Enigma.new
-
-filewriter = File.open(ARGV[1], 'w')
-message_array.each_with_index do |message, i|
-  decrypted_hash = enigma.decrypt(message, ARGV[2], ARGV[3])
-  if i == (message_array.size - 1)
-    binding.pry
-  end
-  filewriter.write(decrypted_hash[:decryption])
-  if i == (message_array.size - 1)
-    puts "Created #{ARGV[1]} " +
-         "with the key #{decrypted_hash[:key]} " +
-         "and date #{decrypted_hash[:date]}"
-  end
-end
+writer = File.open(ARGV[1], 'w')
+encrypted_hash = enigma.decrypt(message, ARGV[2], ARGV[3])
+writer.write(encrypted_hash[:decryption])
+puts "Created #{ARGV[1]} " +
+     "with the key #{encrypted_hash[:key]} " +
+     "and date #{encrypted_hash[:date]}"
+file.close
