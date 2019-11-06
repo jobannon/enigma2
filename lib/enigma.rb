ffcf47019a3530f1e5118c7039c8d54687b8019f
Joshua O'bannon <jtobannon@gmail.com>
2019-11-05 19:26:59 -0700

    engima tests and class refactor

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,10 +1,9 @@
 require 'date'
 require_relative './key'
-require_relative './Offset'
+require_relative './offset'
 require 'pry'
 
 class Enigma
-  #class to encrypt and decrypt
   attr_reader :rand_num_array, :shift_array #not sure I want to expose :shift_array for testing
   def initialize
     @alphabet = ("a".."z").to_a << " "
@@ -19,21 +18,21 @@ class Enigma
     this_key = Key.new(key)
     message = message.downcase #per requirements
     @offset.create_offsets(date)
-    shift_array = create_shift_array(@offset, this_key)
+    create_shift_array(@offset, this_key)
     {
-     :encryption => shift_message(message, shift_array),
+     :encryption => shift_message(message, "+"),
      :key => this_key.rand_num_string,
      :date => date
     }
   end
 
-  def decrypt(message, key, date = Date.now)
+  def decrypt(message, key, date = Time.now.strftime("%d%m%Y"))
     this_key = Key.new(key)
     message = message.downcase #per requirements
     @offset.create_offsets(date)
-    shift_array = create_shift_array(@offset, this_key)
+    create_shift_array(@offset, this_key)
     {
-      :decryption => shift_message(message, shift_array, '-'),
+      :decryption => shift_message(message, '-'),
       :key => this_key.rand_num_string,
       :date => date
     }
@@ -46,10 +45,15 @@ class Enigma
     @shift_array << (offset.d_offset + keyholder.d_key)
   end
 
-  def shift_message(message, shift_array, operator = '+')
+  def shift_message(message, operator)
+    bang_shift_array = @shift_array
     ciper_message = message.chars.reduce([]) do |acc, message_char|
-      acc << @alphabet[((@alphabet.index(message_char).send(operator, @shift_array.first))%27)]
-      @shift_array.rotate!
+      if !(@alphabet.include?(message_char))
+        acc << message_char
+      else
+        acc << @alphabet[((@alphabet.index(message_char).send(operator, bang_shift_array.first)) % 27)]
+        bang_shift_array.rotate!
+      end
       acc
     end
     ciper_message.join
--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -24,29 +24,21 @@ class EnigmaTest < Minitest::Test
       }
     ##test with 3 args
     assert_equal expected, @enigma.encrypt("hello world", "02715", "040895")
-    #### test for date is today's
-    #### test 3 possiblity by length = message length
-    # assert @enigma.offset.expects(:create_offsets)
     #
-    # ###test with todays date
-    # expectedtoday = {
-    #   encryption: expects,
+    # binding.pry
+    #
+    # Time.now.expects
+    # expected2 ={
+    #   encryption: "keder ohulw",
     #   key: "02715",
-    #   date: Time.now.strftime("%d%m%Y")
+    #   date: Time.now.strftime("%d%m%Y"
     #   }
-    # assert_equal expectedtoday, @enigma.encrypt("hello world", "02715")
+    # assert_equal self[:date] = Time.now.strftime("%d%m%Y") ,@enigma.encrypt("hello world", "02715")
     #
-    #
-    # ###test with random key and todays date
-    # expected_today_and_random = {
-    #
-    #
-    # }
-    # assert_equal expected_today_and_random, @enigma.encrypt("hello world")
+    # assert_equal
   end
 
   def test_decrypt
-
     expected = {
       decryption: "hello world",
       key: "02715",
@@ -71,8 +63,7 @@ class EnigmaTest < Minitest::Test
     this_key = Key.new("02715")
     @offset_pass.create_offsets(date)
     @enigma.create_shift_array(@offset_pass, this_key)
-
-    assert_equal "keder ohulw", @enigma.shift_message("hello world", @enigma.shift_array, "+")
+    assert_equal "keder ohulw!", @enigma.shift_message("hello world!", "+")
   end
 
   def test_shift_message_minus
@@ -81,6 +72,6 @@ class EnigmaTest < Minitest::Test
     @offset_pass.create_offsets(date)
     @enigma.create_shift_array(@offset_pass, this_key)
     # binding.pry
-    assert_equal "hello world", @enigma.shift_message("keder ohulw", @enigma.shift_array, "-")
+    assert_equal "hello world!", @enigma.shift_message("keder ohulw!", "-")
   end
 end
